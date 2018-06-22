013962e4b2f8139be5b15ea7c800245d370e5d29
tosoto <tomoto@o2.pl>
2018-06-23 00:10:25 +0200

    Java plugin missing semicolon fix

--- plugins/output_java.py
+++ plugins/output_java.py
@@ -59,7 +59,7 @@ def run(parameters):
                     write('    public static void main(String[] args) {\n\n')
 
             else:
-                scenario_file.write('        System.out.println("%s. %s ---> %s")\n' % (step.id, step.action.label.replace('\n', ''), step.node.label.replace('\n', '')))
+                scenario_file.write('        System.out.println("%s. %s ---> %s");\n' % (step.id, step.action.label.replace('\n', ''), step.node.label.replace('\n', '')))
                 for codeLine in step.action.code.split('\n'):
                     scenario_file.write('        %s\n' % codeLine)
                 for nodeCodeLine in step.node.code.split('\n'):
